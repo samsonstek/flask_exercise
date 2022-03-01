e958217e8e32f5d197cd5f26341c3c05f41ce03b
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-01 17:32:46 +0300

    added failure.html and a jinja for loop

--- sports_registration/app.py
+++ sports_registration/app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "Basketball",
+    "Football",
+    "Tennis",
+    "Volleyball"
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+        return render_template("failure.html")
     return render_template("success.html")
